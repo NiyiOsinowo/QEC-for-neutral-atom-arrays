b687a31af03002021748f9b1988b6553f4a19504
Niyi1 <77877466+NiyiOsinowo@users.noreply.github.com.>
2026-03-13 13:43:51 +0000

    Literature Review

---
+++ QEC_code.py
@@ -0,0 +1,7 @@
+class QEC_code:
+    def __init__(self, code_name):
+        self.code_name = code_name
+
+    def error_plot(self, error_rates):
+        # Placeholder for error plotting logic
+        pass
